c545c3403d70ed730cdaf35d8ffc0a4543a0c36f
Bjorn Linder <bjornishungry@hotmail.com>
2014-02-27 13:30:55 -0500

    Kickball website on Sinatra

--- server.rb
+++ server.rb
@@ -1,24 +1,25 @@
 require 'sinatra'
 require 'CSV'
+require 'pry'
 
-set :public_folder, File.dirname(__FILE__) + '/public'
 set :views, File.dirname(__FILE__) + '/views'
+set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/:path' do
 
   @members = []
 
-  Members = Struct.new(:index_value,:first, :last, :position, :team)
+  Members_struct = Struct.new(:index_value,:first, :last, :position, :team)
   index_value = 0
   CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-    member = Members.new(index_value,row[0],row[1],row[2],row[3])
+    member = Members_struct.new(index_value,row[0],row[1],row[2],row[3])
     @members << member
     index_value += 1
   end
 
   @teams = []
   @positions = []
-
+  
   @members.each do |member|
     unless @teams.include?(member[:team])
       @teams << member[:team]
@@ -27,17 +28,20 @@ get '/:path' do
       @positions << member[:position]
     end
   end
-
-  @path_formatted=params[:path].to_s.gsub(/-/, ' ').split.map(&:capitalize).join(' ')
-
-  def find_player_info(index,path_formatted)
-    if @teams.include?(path_formatted)
-        @members[index][:position]
-    else
-      @positions.include?(path_formatted)
-       @members[index][:team]
+  
+  if params[:path]=='index'
+    @path_formatted='Cartoon League Kickball'
+  else 
+    @path_formatted=params[:path].to_s.gsub(/-/, ' ').split.map(&:capitalize).join(' ')
+    def find_player_info(index,path_formatted)
+      if @teams.include?(path_formatted)
+          @members[index][:position]
+      else
+        @positions.include?(path_formatted)
+         @members[index][:team]
+      end
     end
   end
-
+  
   erb :index
 end
